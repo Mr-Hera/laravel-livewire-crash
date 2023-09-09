536b2d12c2176b6f7938237c8b9b660937d45755
Mr-Hera <randellhera@gmail.com>
2023-09-09 22:02:30 +0300

    Feat: added livewire view + controller, tested public variables

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    Hello from Livewire<hr />
+    {{ $quote }}
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Livewired</title>
+    <title>Laravel Livewire</title>
+    @livewireStyles
 </head>
 <body>
-    
+    <h1>Livewire Crash Course</h1>
+    <livewire:helloWorld />
+
+    @livewireScripts
 </body>
 </html>
