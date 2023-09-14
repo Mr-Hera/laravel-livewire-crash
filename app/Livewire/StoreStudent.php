ab1439900e983998fbe118cd97e42e549e69c3c4
Mr-Hera <randellhera@gmail.com>
2023-09-14 17:47:24 +0300

    Feat: updating data with livewire

--- app/Livewire/StoreStudent.php
+++ app/Livewire/StoreStudent.php
@@ -9,7 +9,7 @@ use Livewire\WithFileUploads;
 class StoreStudent extends Component
 {
     use WithFileUploads;
-    public $name, $email, $image, $student;
+    public $name, $email, $image, $student, $s_id, $update=false, $imageUp;
 
     public function saveData() {
         $student = new Student();
@@ -21,6 +21,24 @@ class StoreStudent extends Component
         $student->save();
 
         $this->resetData();
+        $this->mount();
+    }
+
+    public function upData() {
+        $data = Student::find($this->s_id);
+        $data->name = $this->name;
+        $data->email = $this->email;
+
+        if($this->imageUp) {
+            $imageName = $this->imageUp->store('photos', 'public');
+            $data->image = $imageName;
+        }
+
+        $data->save();
+        $this->update = false;
+
+        $this->resetData();
+        $this->mount();
     }
 
     public function resetData() {
@@ -29,6 +47,16 @@ class StoreStudent extends Component
 
     public function mount() {
         $this->student = Student::all();
+    }
+
+    public function updateStudent($id) {
+        $student = Student::find($id);
+
+        $this->s_id = $student->id;
+        $this->name = $student->name;
+        $this->email = $student->email;
+        $this->image = $student->image;
+        $this->update = true;
     }
 
     public function deleteStudent($id) {
--- resources/views/livewire/store-student.blade.php
+++ resources/views/livewire/store-student.blade.php
@@ -1,44 +1,70 @@
 <div>
-    <h2>Store Student Data</h2>
 
-    <form action="#" wire:submit.prevent="saveData">
-        <div class="">
-            <label for="name">Name</label>
-            <input type="text" name="name" wire:model="name" />
-        </div>
-        <div class="">
-            <label for="email">Email</label>
-            <input type="email" name="email" wire:model="email" />
-        </div>
-        <div class="">
-            <label for="image">Image</label>
-            <input type="file" name="image" wire:model="image" />
-        </div>
-        <div class="">
-            <button>Submit</button>
-        </div>
-    </form>
+    @if ($update == false)
+        <h2>Store Student Data</h2>
 
-    <table border="1px">
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Image</th>
-            <th>Actions</th>
-        </tr>
+        <form action="#" wire:submit.prevent="saveData">
+            <div class="">
+                <label for="name">Name</label>
+                <input type="text" wire:model="name" />
+            </div>
+            <div class="">
+                <label for="email">Email</label>
+                <input type="email" wire:model="email" />
+            </div>
+            <div class="">
+                <label for="image">Image</label>
+                <input type="file" wire:model="image" />
+            </div>
+            <div class="">
+                <button>Submit</button>
+            </div>
+        </form>
 
-        
-        @foreach ($student as $student)
+        <table border="1px">
             <tr>
-                <td>{{ $student->name }}</td>
-                <td>{{ $student->email }}</td>
-                <td class="">
-                    <img src="{{ Storage::url($student->image) }}" height="200" width="300" alt="">
-                </td>
-                <td>
-                    <button style="cursor: pointer" wire:click="deleteStudent({{ $student->id }})">Delete</button>
-                </td>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Image</th>
+                <th>Actions</th>
             </tr>
-        @endforeach
-    </table>
+
+            
+            @foreach ($student as $student)
+                <tr>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td class="">
+                        <img src="{{ Storage::url($student->image) }}" height="200" width="300" alt="">
+                    </td>
+                    <td>
+                        <button style="cursor: pointer" wire:click="updateStudent({{ $student->id }})">Update</button>
+                        <button style="cursor: pointer" wire:click="deleteStudent({{ $student->id }})">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    @else
+        <h2>Update Student Data</h2>
+
+        <form action="#" wire:submit.prevent="upData">
+            <input type="text" wire:model="s_id" hidden />
+            <div class="">
+                <label>Name</label>
+                <input type="text" wire:model="name" />
+            </div>
+            <div class="">
+                <label>Email</label>
+                <input type="email" wire:model="email" />
+            </div>
+            <div class="">
+                <label>Choose New Image</label>
+                <input type="file" wire:model="image" />
+            </div>
+            <div class="">
+                <button>Submit</button>
+            </div>
+        </form>
+    @endif
+    
 </div>
